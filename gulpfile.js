7efee0ec3fed95eb268c93b55b8bb669507c4cb3
Francisco Lucas <franciscolucas686@gmail.com>
2024-06-03 21:19:27 -0300

    comprimindo o js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function compileSass() {
     return gulp.src('./src/style/*.scss')
@@ -17,10 +24,12 @@ function images() {
 function watchFiles() {
     gulp.watch('./src/style/*.scss', gulp.parallel(compileSass))
     gulp.watch('./src/image/**/*', gulp.parallel(images))
+    gulp.watch('./src/js/*.js', gulp.parallel(scripts))
 }
 
 exports.compileSass = compileSass;
 exports.watch = watchFiles;
 exports.images = images;
+exports.scripts = scripts;
 
-exports.default = gulp.parallel(compileSass, watchFiles,images);
+exports.default = gulp.parallel(compileSass, watchFiles,images,scripts);
